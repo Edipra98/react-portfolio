044685fed5df1ba368ee5ccd4f02b6ff51c9725b
Daniel Arpide <djarpide@gmail.com>
2022-07-21 13:12:26 -0400

    testing a fix

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-
-const path = require('path');
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
-});
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.listen(port, () => {
-	console.log(`Server is up on port ${port}!`);
+// create a GET route
+app.get('/*', (req, res) => {
+	res.send(res.sendFile(path.join(__dirname, '../src/index.html')));
 });
