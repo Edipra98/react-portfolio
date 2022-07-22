3773d0f6d32c1c399db84470cb69af9941dd611d
Daniel Arpide <djarpide@gmail.com>
2022-07-22 17:01:52 -0400

    fixed overlay gradient

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,26 +3,28 @@ import React from 'react';
 const Home = () => {
 	return (
 		<React.Fragment>
-			<div className='quote'>
-				<div className='quote-inside'>
-					Many of life’s failures are people who did not realize how close they
-					were to success when they gave up.
+			<div className='center-center'>
+				<div className='quote'>
+					<div className='quote-inside'>
+						Many of life’s failures are people who did not realize how close
+						they were to success when they gave up.
+					</div>
+					<div className='author'>&mdash; Thomas A. Edison &mdash;</div>
 				</div>
-				<div className='author'>&mdash; Thomas A. Edison &mdash;</div>
-			</div>
-			<div className='about'>
-				<p className='about-me'>About me:</p>
-				<i className='tab'>
-					&ensp;My name is Daniel Arpide and I'm a graduate of Wentworth
-					Institute of Technology with a degree in Computer Engineering. While
-					having a curriculum based in both Computer Science and computer
-					hardware, I have always had a stronger interest in the Computer
-					Science side of the courses. In college the largest project I was a
-					part of was when I worked with a team to develop an application using
-					the Spotify API, while creating this I found out that programming was
-					the path I wanted to take in my career. I'm currently junior software
-					engineer focusing in web development.
-				</i>
+				<div className='about'>
+					<p className='about-me'>About me:</p>
+					<i className='tab'>
+						&ensp;My name is Daniel Arpide and I'm a graduate of Wentworth
+						Institute of Technology with a degree in Computer Engineering. While
+						having a curriculum based in both Computer Science and computer
+						hardware, I have always had a stronger interest in the Computer
+						Science side of the courses. In college the largest project I was a
+						part of was when I worked with a team to develop an application
+						using the Spotify API, while creating this I found out that
+						programming was the path I wanted to take in my career. I'm
+						currently junior software engineer focusing in web development.
+					</i>
+				</div>
 			</div>
 		</React.Fragment>
 	);
